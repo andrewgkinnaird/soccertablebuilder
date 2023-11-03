04f581d627d02693d6c8c423ec6681e5d485f9d1
andrewgkinnaird <andy_kinnaird@yahoo.co.uk>
2023-11-03 14:50:30 +0000

    creating a react structure

---
+++ soccertablesimulator/src/Components/App/App.js
@@ -0,0 +1,40 @@
+
+import './App.css';
+import * as React from 'react';
+import {Button,Table,TableContainer,TableHead,TableBody,TableCell,TableRow,Paper} from '@material-ui/core';
+import {englishPremierTeams} from '../../testData';
+
+function App() {
+  return (
+    <div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Club</TableCell>
+            <TableCell align="right">P</TableCell>
+            <TableCell align="right">W</TableCell>
+            <TableCell align="right">D</TableCell>
+            <TableCell align="right">L</TableCell>
+            <TableCell align="right">GF</TableCell>
+            <TableCell align="right">GA</TableCell>
+            <TableCell align="right">Points</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            {englishPremierTeams.map((team) => (
+              
+              
+                <TableRow>{team}</TableRow>
+              
+            ))} 
+        </TableBody>
+      </Table>
+    </TableContainer>
+    <Button variant="contained" color="primary">Simulate</Button>
+    </div>
+  );
+}
+
+
+export default App;
